35ffb35e2e8e3a4d177ef2221e7dc0d4aa77a8ee
cristian contreras <cr.ontreras@gmail.com>
2023-08-19 17:20:01 -0500

    class 4. usestate

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -2,6 +2,23 @@ import React from "react"
 
 function UseState ({ name }) {
     const [error, setError] = React.useState(true)
+    const [loading, setLoading] = React.useState(false)
+
+    React.useEffect(() => {
+        console.log('Empezando Efecto')
+        
+        if (loading) {
+            setTimeout(() => {
+                console.log('Empenzando Validación')
+    
+                setLoading(false)
+    
+                console.log('Terminando Validación')
+            }, 3000)
+        }
+
+        console.log('Terminando Efecto')
+    }, [loading])
 
     return (
         <div>
@@ -10,13 +27,15 @@ function UseState ({ name }) {
             <p>Escribe el código de seguridad</p>
 
             { error && <p>Error: La cagaste</p> }
+            
+            { loading && <p>Loading: Está cargando, mi brother</p> }
 
             <input 
                 placeholder="Código de Seguridad"
             />
 
             <button 
-                onClick={ () => setError(!error)}
+                onClick={ () => setLoading(true) }
             > 
                 Dame Click. PLEASE!!!! 
             </button>
